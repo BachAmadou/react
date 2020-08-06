24d3f1f304aba24926ea8780e3343ec8514981d5
Bach <bafosth@yahoo.fr>
2020-08-06 11:39:11 -0400

    added description to readme.md file

--- 1-Pomodoro/src/App.js
+++ 1-Pomodoro/src/App.js
@@ -1,23 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './App.css';
 
 export default function App() {
 
   const [title, setTitle] = useState('LET THE COUNTDOWN BEGIN!');
   const [timeLeft, setTimeLeft] = useState(10);
+  // padStart: js function that adds a number to the beginning of another number
   const minutes = Math.floor(timeLeft / 60).toString().padStart(2, 0); 
   const seconds = (timeLeft - minutes * 60).toString().padStart(2, 0);
-  // padStart: js function that adds a number to the beginning of another number
+  const intervalRef = useRef(null);
 
 
   function startTimer() {
-    setInterval(() => {
+    if(intervalRef.current !== null) return;
+
+    intervalRef.current = setInterval(() => {
       setTimeLeft(timeLeft => {
         if(timeLeft >= 1) return timeLeft - 1;
+        resetTimer();
         return 0;
       });
     }, 1000);
   }
+
+  function stopTimer() {
+    if(intervalRef.current === null) return;
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('KEEP IT UP!');
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('READY TO GO ANOTHER ROUND');
+    setTimeLeft(25 * 60);
+  }
+  
 
   return (
     <div className="app">
@@ -31,8 +51,8 @@ export default function App() {
 
       <div className="buttons">
         <button onClick={startTimer}>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
