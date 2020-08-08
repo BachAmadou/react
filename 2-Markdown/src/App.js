e7a3180b811c4662d296a5f1a5ccbec84c3b2d90
Bach <bafosth@yahoo.fr>
2020-08-07 21:33:53 -0400

    added markdown project

---
+++ 2-Markdown/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './App.css';
+
+export default function App() {
+
+  const [markdown, setMarkdown] = useState("# Cool");
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
+  return (
+    <div className="app">
+      <textarea 
+      onChange={handleChange} 
+      value={markdown} 
+      />
+
+    <ReactMarkdown className="preview" source={markdown} />
+    </div>
+  );
+}
