6c8afa4f8e55ead830a4c9448cf6ea00d06d8f04
BachAmadou <bafosth@yahoo.fr>
2020-09-05 19:49:02 -0400

    added useEFfect for canvas width-height, for moving the box and listening to key press

--- 5-Moving-boxes-and-links/src/App.js
+++ 5-Moving-boxes-and-links/src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const canvasRef = useRef(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  // set the height and width of canvas
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+  }, []);
+
+  // move the box with x or y changes
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.clearRect(0, 0, window.innerHeight, window.innerWidth);
+    context.fillRect(x, y, 100, 100);
+  }, [x, y]);
+
+// add event listener to window to listen for arrow keys
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    function handleKeyDown(e) {
+      console.log(e.keyCode, e);
+      if(e.key === 'ArrowUp') setY((y) => y - 20 );
+      if(e.key === 'ArrowLeft') setX((x) => x - 20 );
+      if(e.key === 'ArrowDown') setY((y) => y + 20 );
+      if(e.key === 'ArrowRight') setX((x) => x + 20 );
+    }
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
   return (
     <div className="app">
-      <canvas />
+      <canvas ref={canvasRef} />
 
       <div className="arrows">
-        <button>Up</button>
-        <button>Left</button>
-        <button>Down</button>
-        <button>Right</button>
+        <button onClick={() => setY((y) => y - 20 )}>Up</button>
+        <button onClick={() => setX((x) => x - 20 )}>Left</button>
+        <button onClick={() => setY((y) => y + 20 )}>Down</button>
+        <button onClick={() => setX((x) => x + 20 )}>Right</button>
       </div>
 
       <div className="images">
